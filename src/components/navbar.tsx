0abbf13a2028b7f78092270afac7812aa560ec6f
rich-info <ridvan@rich-informatik.de>
2024-02-21 21:26:55 +0100

    Add navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const [visible, setVisible] = useState(false);
+
+  const handleNavIconClick = () => {
+    setVisible(!visible);
+  };
+
+  const navItems = [
+    { id: 1, text: "Home", link: "/" },
+    { id: 2, text: "Products", link: "/products" },
+    { id: 3, text: "Categories", link: "/categories" },
+    { id: 4, text: "About", link: "/about" },
+  ];
+
+  return (
+    <nav className="flex justify-between items-center h-24 mx-auto px-4 text-white">
+      <NavLink to="/" className="flex">
+        <svg
+          width="40"
+          height="40"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="#fff"
+        >
+          <polygon
+            points="20,5 35,15 35,35 20,45 5,35 5,15"
+            transform="scale(0.7)"
+          />
+        </svg>
+        <span className="w-full text-3xl font-bold">React CRUD Demo</span>
+      </NavLink>
+
+      <div className="hidden md:flex">
+        {navItems.map((item) => (
+          <NavLink
+            key={item.id}
+            className="p-6 hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-black"
+            to={item.link}
+          >
+            {item.text}
+          </NavLink>
+        ))}
+      </div>
+
+      <div onClick={handleNavIconClick} className="block md:hidden">
+        {visible ? <span>Hide</span> : <span>Nav</span>}
+      </div>
+
+      <div
+        className={
+          visible
+            ? "flex flex-col fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]"
+        }
+      >
+        <NavLink to="/" className="flex py-6 px-2">
+          <svg
+            width="40"
+            height="40"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#fff"
+          >
+            <polygon
+              points="20,5 35,15 35,35 20,45 5,35 5,15"
+              transform="scale(0.7)"
+            />
+          </svg>
+          <span className="w-full text-3xl font-bold">React CRUD Demo</span>
+        </NavLink>
+        {navItems.map((item) => (
+          <NavLink
+            key={item.id}
+            onClick={handleNavIconClick}
+            className="p-4 border-b  hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600"
+            to={item.link}
+          >
+            {item.text}
+          </NavLink>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,28 +1,16 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/navbar";
 
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/products">Products</Link>
-          </li>
-          <li>
-            <Link to="/categories">Categories</Link>
-          </li>
-        </ul>
-      </nav>
+      <header>
+        <Navbar />        
+      </header>
 
       <Outlet />
     </>
-  )
+  );
 };
 
 export default Layout;
