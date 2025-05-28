0765b3004ff18c6808681dd900a7adaa9652ea2f
rich-info <ridvan@rich-informatik.de>
2025-05-28 23:48:43 +0200

    Update table skeleton for larger tables

--- src/components/table-skeleton.tsx
+++ src/components/table-skeleton.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import Skeleton from './skeleton';
 
 const TableSkeleton: React.FC = () => {
   return (
     <div>
-      <Skeleton className="h-8 w-48 mb-4" /> {/* Title skeleton */}
-      <div className="space-y-3">
-        <div className="flex gap-4">
-          <Skeleton className="h-6 w-16" /> {/* ID column header */}
-          <Skeleton className="h-6 w-32" /> {/* Name column header */}
-        </div>
-        {[...Array(2)].map((_, index) => (
-          <div key={index} className="flex gap-4">
-            <Skeleton className="h-6 w-16" /> {/* ID cell */}
-            <Skeleton className="h-6 w-32" /> {/* Name cell */}
-          </div>
-        ))}
+      <div className="flex justify-between items-center mb-4">
+        <div className="h-8 w-48 bg-gray-200 rounded animate-pulse"></div>
+        <div className="h-10 w-40 bg-gray-200 rounded animate-pulse"></div>
       </div>
+      <table className="w-full">
+        <tbody>
+          {[...Array(2)].map((_, index) => (
+            <tr key={index} className="border-t">
+              <td className="p-2">
+                <div className="h-4 w-12 bg-gray-200 rounded animate-pulse"></div>
+              </td>
+              <td className="p-2">
+                <div className="h-4 w-32 bg-gray-200 rounded animate-pulse"></div>
+              </td>
+              <td className="p-2 text-right">
+                <div className="h-5 w-5 bg-gray-200 rounded animate-pulse inline-block"></div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
